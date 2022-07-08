ae6a60b4919d1c64da3277a32896d10221731df5
houbb <houbb@neocrm.com>
2022-07-08 09:27:50 +0800

    删除无用文件&添加语音demo

--- pages/test.js
+++ pages/test.js
@@ -1,7 +1,7 @@
-/*
- * @Author: houbb
- * @Date: 2022-07-04 09:45:51
- * @LastEditTime: 2022-07-04 09:45:51
- * @LastEditors: houbb
- * @Description:
- */
+export default function Home() {
+  return (
+    <div>
+
+    </div>
+  )
+}
---
+++ pages/voice.js
@@ -0,0 +1,30 @@
+
+export default function Home() {
+  const queryParams = {
+    // voiceURI: 'Ting-Ting',
+    // lang: 'zh-CN',
+    // volume: 1,
+    // pitch: 1,
+    // rate: 1,
+    text: '大扎好，我西渣渣辉。'
+}
+  const  onSpeak = () => {
+    const speechInstance = new SpeechSynthesisUtterance();
+    Object.keys(queryParams).forEach(key => {
+      speechInstance[key] = queryParams[key];
+    })
+    console.log(speechInstance);
+    typeof speechSynthesis !== "undefined" && speechSynthesis.speak(speechInstance);
+    speechInstance.onstart = () => {
+      console.log('onstart')
+    }
+    speechInstance.onend = () => {
+      console.log('end')
+    }
+  }
+  return (
+    <div>
+       <div onClick={onSpeak}>speak</div>
+    </div>
+  )
+}
